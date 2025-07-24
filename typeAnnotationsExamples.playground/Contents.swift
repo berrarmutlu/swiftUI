e3c2206a4978f976cd1aca47fd5edb09ba7dcfdd
berra armutlu <armutluberra@gmail.com>
2025-07-24 17:12:54 +0300

    type annotations finished

---
+++ typeAnnotationsExamples.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+let surname = "Armutlu"
+var score = 10
+
+let surnm: String = "Armutlu"
+var scr: Int = 1
+var scrDouble: Double = 0
+
+var percentage: Double = 99 //.0 yazmaya gerek olmaz
+
+let playerName: String = "Kate" //string text tutar
+
+var number: Int = 10 //int tam sayi tutar
+
+let pi: Double = 3.14 //double ondalikli sayi tutar
+
+var isStudent: Bool = true //bool true ya da false
+
+var albums: [String] = ["Ugly Duck", "Fish Man", "Sink Water"] //array
+
+var user: [String: String] = ["id": "@utaritt"] //dictionary
+
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"]) //set
+
+var animals: [String] = [String]()
+
+enum months{
+    case january, february, march, april, may, june //Bir enumun değerleri enumun kendisiyle aynı türdedir
+}                                                   //enum adini kaldirip month = .march kullanabiliriz
+var month = months.january
+
+var username: String //burada kullanici adinin string icerecegini belirttik ve kullanmadan once deger atadik
+username = "@vesvesenur"
+print(username)
+    
+
+
+
+
+
